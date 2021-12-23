8a97e75cadbe35e082550467977f3892fe8903a6
Matt Dixon <mdixon4@gmail.com>
2021-12-23 13:57:17 +1100

    render netlify function

---
+++ frenchy/netlify/functions/render/render.js
@@ -0,0 +1,25 @@
+const chromium = require('chrome-aws-lambda')
+
+exports.handler = async (event) => {
+
+  const pageToScreenshot = event.queryStringParameters.url;
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless
+  })
+  const page = await browser.newPage()
+  await page.goto(pageToScreenshot)
+  const screenshot = await page.screenshot({
+    encoding: 'binary'
+  })
+  await browser.close()
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Complete screenshot of ${pageToScreenshot}`,
+      buffer: screenshot
+    })
+  }
+}
