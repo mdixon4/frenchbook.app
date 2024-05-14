f33d765028685847666eb8cfe12b46b613a2b66a
Matt Dixon <mdixon4@gmail.com>
2024-05-14 11:14:42 +1000

    Fix _util.js which was using typescript

--- netlify/functions/export/_util.js
+++ netlify/functions/export/_util.js
@@ -1,24 +1,24 @@
 
 // convert a Unicode string to a string in which
 // each 16-bit unit occupies only one byte
-export function toBinary(string: string): string {
+export function toBinary(string) {
   const codeUnits = new Uint16Array(string.length);
   for (let i = 0; i < codeUnits.length; i++) {
     codeUnits[i] = string.charCodeAt(i);
   }
   return String.fromCharCode(...new Uint8Array(codeUnits.buffer));
 }
-export function fromBinary(binary: string): string {
+export function fromBinary(binary) {
   const bytes = new Uint8Array(binary.length);
   for (let i = 0; i < bytes.length; i++) {
     bytes[i] = binary.charCodeAt(i);
   }
   return String.fromCharCode(...new Uint16Array(bytes.buffer));
 }
-export const toUrlHash = (utf8String: string): string => {
+export const toUrlHash = (utf8String) => {
   return btoa(toBinary(utf8String))
 }
-export const fromUrlHash = (base64String: string): string => {
+export const fromUrlHash = (base64String) => {
   return fromBinary(atob(base64String))
 }
 
