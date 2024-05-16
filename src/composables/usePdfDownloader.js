90006e0694d0a756c64d17e4983b862f1f1c0972
Matt Dixon <mdixon4@gmail.com>
2024-05-16 23:24:36 +1000

    Prompt for PDF download location

--- src/composables/usePdfDownloader.js
+++ src/composables/usePdfDownloader.js
@@ -3,11 +3,53 @@ import { useSongStore } from '../store/song'
 import { useUIStore } from '../store/ui'
 import { useLocalFileStore } from '../store/localFile'
 
+const fetchPdf = async () => {
+  const songStore = useSongStore()
+  const uiStore = useUIStore()
+  const resp = await fetch('/api/export', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+      'X-Api-Key': uiStore.pdfApiKey,
+    },
+    body: songStore.songText,
+  })
+  if (resp.status === 200 && resp.headers.get('Content-Type') === 'application/pdf' && resp.headers.get('Content-Disposition').startsWith('attachment')) {
+    return resp
+  }
+  return null
+}
+
+const promptForSave = (filename) => {
+  return window.showSaveFilePicker({
+    suggestedName: filename,
+    types: [
+      {
+        description: 'PDF files',
+        accept: {
+          'application/pdf': ['.pdf']
+        }
+      }
+    ]
+  })
+}
+
+const autoDownload = async (resp, filename) => {
+  const blob = await resp.blob()
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement
+  a.href = url
+  a.download = filename
+  a.click()
+  URL.revokeObjectURL(url)
+  alert('downloading...')
+}
 
 export const usePdfDownloader = () => {
   const songStore = useSongStore()
   const uiStore = useUIStore()
   const localFileStore = useLocalFileStore()
+  const canSave = 'showOpenFilePicker' in window
 
   const canExport = computed(() => {
     return songStore.songText && uiStore.pdfApiKey
@@ -27,29 +69,33 @@ export const usePdfDownloader = () => {
   const toPdf = async () => {
     console.log('Exporting to PDF')
     if (!canExport.value) return
+    const NOT_SUPPORTED = Symbol('NOT_SUPPORTED')
 
-    const resp = await fetch('/api/export', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain',
-        'X-Api-Key': uiStore.pdfApiKey,
-      },
-      body: songStore.songText,
-    })
+    const [resp, fileHandle] = await Promise.all([
+      fetchPdf(),
+      canSave ? promptForSave(pdfFileName.value) : Promise.resolve(NOT_SUPPORTED)
+    ])
 
-    if (resp.status === 200 && resp.headers.get('Content-Type') === 'application/pdf' && resp.headers.get('Content-Disposition').startsWith('attachment')) {
-      // Prompt for download
-      const blob = await resp.blob()
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      a.href = url
-      a.download = pdfFileName.value
-      a.click()
-      URL.revokeObjectURL(url)
+    if (!fileHandle) {
       return
     }
 
-    console.error('Failed to export to PDF', resp)
+    if (!resp) {
+      alert('Failed to export PDF')
+      return
+    }
+
+    if (fileHandle === NOT_SUPPORTED) {
+      await autoDownload(resp, pdfFileName.value)
+      return
+    }
+
+    if (fileHandle instanceof FileSystemFileHandle) {
+      const writableStream = await fileHandle.createWritable()
+      const blob = await resp.blob()
+      writableStream.write(blob)
+      writableStream.close()
+    }
   }
 
   return {
